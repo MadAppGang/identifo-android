19f1b12a4e0a206d3c91168465593cd96e2f10e5
Eugene Prytula <ep@madappgang.com>
2021-02-26 15:48:56 +0200

    Add request to entrance via username/password

--- identifoLibUi/src/main/java/com/prytula/identifolibui/login/CommonLoginFragment.kt
+++ identifoLibUi/src/main/java/com/prytula/identifolibui/login/CommonLoginFragment.kt
@@ -3,9 +3,16 @@ package com.prytula.identifolibui.login
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.prytula.IdentifoAuth
+import com.prytula.identifolib.extensions.onError
+import com.prytula.identifolib.extensions.onSuccess
 import com.prytula.identifolibui.R
+import com.prytula.identifolibui.extensions.showMessage
 
 
 /*
@@ -14,9 +21,30 @@ import com.prytula.identifolibui.R
  */
 
 class CommonLoginFragment : Fragment(R.layout.fragment_common_login) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.buttonLoginWithPhoneNumber).setOnClickListener {
+
+        val rootView = view.findViewById<ConstraintLayout>(R.id.constraint_login_root)
+        val usernameEditText = view.findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val passwordEditText = view.findViewById<EditText>(R.id.editTextPassword)
+        val loginButton = view.findViewById<Button>(R.id.buttonLogin)
+        val loginWithPhoneNumber = view.findViewById<Button>(R.id.buttonLoginWithPhoneNumber)
+
+        loginButton.setOnClickListener {
+            lifecycleScope.launchWhenCreated {
+                val username = usernameEditText.text.toString()
+                val password = passwordEditText.text.toString()
+
+                IdentifoAuth.loginWithUsernameAndPassword(username, password).onError {
+                    rootView.showMessage(it.error.message)
+                }.onSuccess {
+                    requireActivity().finish()
+                }
+            }
+        }
+
+        loginWithPhoneNumber.setOnClickListener {
             findNavController().navigate(R.id.action_commonLoginFragment_to_phoneNumberLoginFragment)
         }
     }
