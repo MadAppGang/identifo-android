526e0c03af20b852462b57825d852477292267d4
Eugene Prytula <ep@madappgang.com>
2021-03-01 13:05:27 +0200

    Add google auth sdk and ask the federated token

--- identifoLibUi/src/main/java/com/prytula/identifolibui/login/CommonLoginFragment.kt
+++ identifoLibUi/src/main/java/com/prytula/identifolibui/login/CommonLoginFragment.kt
@@ -1,5 +1,6 @@
 package com.prytula.identifolibui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,11 +9,21 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.prytula.IdentifoAuth
+import com.prytula.identifolib.extensions.handleResult
 import com.prytula.identifolib.extensions.onError
 import com.prytula.identifolib.extensions.onSuccess
 import com.prytula.identifolibui.R
 import com.prytula.identifolibui.extensions.showMessage
+import java.lang.Exception
 
 
 /*
@@ -22,14 +33,36 @@ import com.prytula.identifolibui.extensions.showMessage
 
 class CommonLoginFragment : Fragment(R.layout.fragment_common_login) {
 
+    companion object {
+        private const val RC_SIGN_IN = 1
+    }
+
+    private lateinit var rootView : ConstraintLayout
+
+    private val googleOptions: GoogleSignInOptions by lazy {
+        // TODO: Add passing of app id
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("153119406654-a2uoase99mdskhsrs406v5g7l9bp7dvc.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+    }
+
+    private val googleClient: GoogleSignInClient by lazy {
+        GoogleSignIn.getClient(
+            requireActivity(),
+            googleOptions
+        )
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val rootView = view.findViewById<ConstraintLayout>(R.id.constraint_login_root)
+        rootView = view.findViewById(R.id.constraint_login_root)
         val usernameEditText = view.findViewById<EditText>(R.id.editTextTextEmailAddress)
         val passwordEditText = view.findViewById<EditText>(R.id.editTextPassword)
         val loginButton = view.findViewById<Button>(R.id.buttonLogin)
         val loginWithPhoneNumber = view.findViewById<Button>(R.id.buttonLoginWithPhoneNumber)
+        val loginWithGoogle = view.findViewById<SignInButton>(R.id.buttonLoginWithGoogle)
 
         loginButton.setOnClickListener {
             lifecycleScope.launchWhenCreated {
@@ -44,8 +77,35 @@ class CommonLoginFragment : Fragment(R.layout.fragment_common_login) {
             }
         }
 
+        loginWithGoogle.setOnClickListener {
+            signIn()
+        }
+
         loginWithPhoneNumber.setOnClickListener {
             findNavController().navigate(R.id.action_commonLoginFragment_to_phoneNumberLoginFragment)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask : Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            val idToken = account!!.idToken
+            rootView.showMessage("Token - ${idToken.toString()}")
+        } catch (e : Exception) {
+            rootView.showMessage(e.message.toString())
+        }
+    }
+
+    private fun signIn() {
+        val intent = googleClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
     }
 }
