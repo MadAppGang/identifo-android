d0b74894c09a1c5b55b81c52a07ded8d972868c9
Eugene Prytula <ep@madappgang.com>
2021-03-08 16:13:08 +0200

    Add country picker to facilitate picking of number

--- identifoLibUi/src/main/java/com/prytula/identifolibui/login/phoneNumber/PhoneNumberLoginFragment.kt
+++ identifoLibUi/src/main/java/com/prytula/identifolibui/login/phoneNumber/PhoneNumberLoginFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.hbb20.CountryCodePicker
 import com.prytula.IdentifoAuth
 import com.prytula.identifolib.extensions.onError
 import com.prytula.identifolib.extensions.onSuccess
@@ -25,20 +28,30 @@ class PhoneNumberLoginFragment : Fragment(R.layout.fragment_phone_number_login) 
         super.onViewCreated(view, savedInstanceState)
 
         val rootView = view.findViewById<ConstraintLayout>(R.id.constraint_phone_number_root)
-        val phoneNumberEditText = view.findViewById<EditText>(R.id.editTextPhone)
+        val phoneNumberEditText = view.findViewById<EditText>(R.id.textInputPhoneNumber)
+        val numberPrefixView = view.findViewById<CountryCodePicker>(R.id.countryCodePicker)
         val buttonProceed = view.findViewById<Button>(R.id.buttonProceed)
 
+        var isPhoneNumberValid = false
+
+        numberPrefixView.registerCarrierNumberEditText(phoneNumberEditText)
+        numberPrefixView.setPhoneNumberValidityChangeListener { isPhoneNumberValid = it }
+
         buttonProceed.setOnClickListener {
-            lifecycleScope.launchWhenCreated {
-                val phoneNumber = phoneNumberEditText.text.toString()
-                IdentifoAuth.requestPhoneCode(phoneNumber).onError {
-                    rootView.showMessage(it.error.message)
-                }.onSuccess {
-                    findNavController().navigate(
-                        R.id.action_phoneNumberLoginFragment_to_oneTimePasswordFragment,
-                        OneTimePasswordFragment.putArgument(phoneNumber)
-                    )
+            if (isPhoneNumberValid) {
+                lifecycleScope.launchWhenCreated {
+                    val phoneNumber = numberPrefixView.fullNumberWithPlus
+                    IdentifoAuth.requestPhoneCode(phoneNumber).onError {
+                        rootView.showMessage(it.error.message)
+                    }.onSuccess {
+                        findNavController().navigate(
+                            R.id.action_phoneNumberLoginFragment_to_oneTimePasswordFragment,
+                            OneTimePasswordFragment.putArgument(phoneNumber)
+                        )
+                    }
                 }
+            } else {
+                rootView.showMessage(getString(R.string.phoneNumberInvalid))
             }
         }
     }
