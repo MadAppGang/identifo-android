b09766c99604d002f24914c0d134a3fa3bb40844
Jack <ep@madappgang.com>
2021-03-16 22:03:47 +0200

    Add the ability to choose an account image

--- identifoLibUi/src/main/java/com/prytula/identifolibui/registration/RegistrationFragment.kt
+++ identifoLibUi/src/main/java/com/prytula/identifolibui/registration/RegistrationFragment.kt
@@ -2,10 +2,13 @@ package com.prytula.identifolibui.registration
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.asLiveData
 import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.prytula.identifolibui.R
 import com.prytula.identifolibui.databinding.FragmentRegistrationBinding
 import com.prytula.identifolibui.extensions.onDone
@@ -19,8 +22,23 @@ import com.prytula.identifolibui.extensions.showMessage
 
 class RegistrationFragment : Fragment(R.layout.fragment_registration) {
 
+    companion object {
+        private const val MIMETYPE_IMAGES = "image/*"
+    }
+
     private val registrationBinding by viewBinding(FragmentRegistrationBinding::bind)
     private val registrationViewModel by viewModels<RegistrationViewModel>()
+
+    private val getImageContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
+            imageUri?.let {
+                registrationBinding.imageViewAccount.load(it) {
+                    crossfade(true)
+                    placeholder(R.drawable.ic_account)
+                    transformations(CircleCropTransformation())
+                }
+            }
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,6 +54,10 @@ class RegistrationFragment : Fragment(R.layout.fragment_registration) {
             .observe(viewLifecycleOwner) { errorResponse ->
                 registrationBinding.constraintRegistrationRoot.showMessage(errorResponse.error.message)
             }
+
+        registrationBinding.imageViewAccount.setOnClickListener {
+            getImageContent.launch(MIMETYPE_IMAGES)
+        }
     }
 
     private fun pushUsernameAndPassword() {
