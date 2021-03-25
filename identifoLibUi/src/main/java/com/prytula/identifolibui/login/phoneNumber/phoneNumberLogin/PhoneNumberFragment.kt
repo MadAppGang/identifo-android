c282691eeda88c71239e68c70f47a35090decdd1
Eugene <ep@madappgang.com>
2021-03-25 18:22:15 +0200

    Add redirection to registration flow from phone number screen

--- identifoLibUi/src/main/java/com/prytula/identifolibui/login/phoneNumber/phoneNumberLogin/PhoneNumberFragment.kt
+++ identifoLibUi/src/main/java/com/prytula/identifolibui/login/phoneNumber/phoneNumberLogin/PhoneNumberFragment.kt
@@ -5,9 +5,13 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.load
 import com.prytula.identifolibui.R
 import com.prytula.identifolibui.databinding.FragmentPhoneNumberLoginBinding
 import com.prytula.identifolibui.extensions.showMessage
+import com.prytula.identifolibui.login.IdentifoActivity
+import com.prytula.identifolibui.login.options.LoginOptions
+import com.prytula.identifolibui.login.options.Style
 import com.prytula.identifolibui.login.phoneNumber.oneTimePassword.OneTimePasswordFragment
 
 
@@ -20,19 +24,34 @@ class PhoneNumberFragment : Fragment(R.layout.fragment_phone_number_login) {
 
     private val phoneNumberLoginBinding by viewBinding(FragmentPhoneNumberLoginBinding::bind)
 
+    private val loginOptions: LoginOptions by lazy { (requireActivity() as IdentifoActivity).loginOptions }
+    private val commonStyle: Style? by lazy { loginOptions.commonStyle }
+
+    companion object {
+        private val PHONE_NUMBER_PATTERN = "^[+][0-9]{11,13}\$".toRegex()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var isPhoneNumberValid = false
+        phoneNumberLoginBinding.imageViewBackArrow.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
-        phoneNumberLoginBinding.countryCodePicker.run {
-            registerCarrierNumberEditText(phoneNumberLoginBinding.textInputPhoneNumber)
-            setPhoneNumberValidityChangeListener { isPhoneNumberValid = it }
+        phoneNumberLoginBinding.textViewRegisterNewAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_phoneNumberLoginFragment_to_registrationFragment)
+        }
+
+        commonStyle?.let {
+            phoneNumberLoginBinding.imageViewLogo.load(it.companyLogo)
+            phoneNumberLoginBinding.textViewCompanyName.text = it.companyName
+            phoneNumberLoginBinding.textViewCompanyGreetings.text = it.greetingsText
         }
 
         phoneNumberLoginBinding.buttonProceed.setOnClickListener {
-            if (isPhoneNumberValid) {
-                val phoneNumber = phoneNumberLoginBinding.countryCodePicker.fullNumberWithPlus
+            val phoneNumber = phoneNumberLoginBinding.textInputPhoneNumber.text.toString()
+            val isNumberValid = phoneNumber.matches(PHONE_NUMBER_PATTERN)
+            if (isNumberValid) {
                 findNavController().navigate(
                     R.id.action_phoneNumberLoginFragment_to_oneTimePasswordFragment,
                     OneTimePasswordFragment.putArgument(phoneNumber)
