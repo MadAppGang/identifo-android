97e1299e8994723854c3d160ff2f5a02ba54a66d
Eugene <ep@madappgang.com>
2021-03-29 12:16:24 +0300

    Remove comment

--- identifoLib/src/main/java/com/prytula/IdentifoAuthentication.kt
+++ identifoLib/src/main/java/com/prytula/IdentifoAuthentication.kt
@@ -39,11 +39,6 @@ import org.koin.core.context.startKoin
  * Copyright (c) 2021 MadAppGang. All rights reserved.
  */
 
-/**
- * Class responsible for giving user basic functions of authentication and facilitate work
- * with access tokens and their managing.
- */
-
 object IdentifoAuthentication : KoinComponent {
 
     private val tokenDataStorage by inject<ITokenDataStorage>()
